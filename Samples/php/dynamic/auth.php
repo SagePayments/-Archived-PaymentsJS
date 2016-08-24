e4dab080cf8efed57f1c92057cd65c45762ac8d6
Majid <majid.razvi@sage.com>
2016-08-24 20:44:52 +0000

    updated dynamic sample

--- Samples/php/dynamic/auth.php
+++ Samples/php/dynamic/auth.php
@@ -1,22 +1,34 @@
 <?php
+
     require('../shared/shared.php');
     
+    $nonces = getNonces();
+    
     $requestType = "payment";
+    $requestId = "Invoice" . rand(0, 1000);
+    
     $amount = $_GET["amount"];
     
-    // some arbitrary values for this demo
-    $requestId = "Invoice" . rand(0, 1000);
-    $nonce = uniqid();
-    $postbackUrl = "https://www.example.com/";
+    $req = [
+        "merchantId" => $merchant['ID'],
+        "merchantKey" => $merchant['KEY'], // don't include the Merchant Key in the JavaScript initialization!
+        "requestType" => $requestType,
+        "requestId" => $requestId,
+        "amount" => $amount,
+        "nonce" => $nonces['salt'],
+        // on the other hand, include these here even if you leave them out of the JS init
+        "postbackUrl" => $request['postbackUrl'], // if not specified in the JS init, defaults to the empty string
+        "environment" => $request['environment'], // defaults to "cert"
+        "preAuth" => $request['preAuth'] // defaults to false
+    ]; 
     
-    $combinedString = $requestType . $requestId . $merchantCredentials['MID'] . $postbackUrl . $nonce . $amount;
-    $authKey = createHmac($combinedString, $merchantCredentials["MKEY"]);
+    $authKey = getAuthKey(json_encode($req), $developer['KEY'], $nonces['salt'], $nonces['iv']);
 ?>
 {
     "authKey": "<?php echo $authKey; ?>",
     "invoice": "<?php echo $requestId; ?>",
-    "nonce": "<?php echo $nonce; ?>",
-    "merch": "<?php echo $merchantCredentials['MID']; ?>",
-    "apiKey": "<?php echo $developerId; ?>",
-    "postback": "<?php echo $postbackUrl; ?>"
+    "nonce": "<?php echo $nonces['salt']; ?>",
+    "merch": "<?php echo $merchant['ID']; ?>",
+    "apiKey": "<?php echo $developer['ID'] ?>",
+    "postback": "<?php echo $request['postbackUrl']; ?>"
 }
