8c6560db83020b80d6f76e410cf4e8b2b7e22a42
Majid <majid.razvi@sage.com>
2016-08-24 20:30:12 +0000

    updated require sample

--- Samples/php/require/index.php
+++ Samples/php/require/index.php
@@ -1,18 +1,27 @@
 <?php
     require('../shared/shared.php');
     
+    $nonces = getNonces();
+    
     $requestType = "payment";
-    $amount = "1.00";
+    $requestId = "Invoice" . rand(0, 1000); // this'll be used as the order number
     
-    // some arbitrary values for this demo
-    $requestId = "Invoice" . rand(0, 1000);
-    $nonce = uniqid();
-    $postbackUrl = "https://www.example.com/";
+    $req = [
+        "merchantId" => $merchant['ID'],
+        "merchantKey" => $merchant['KEY'], // don't include the Merchant Key in the JavaScript initialization!
+        "requestType" => $requestType,
+        "requestId" => $requestId,
+        "amount" => $request['amount'],
+        "nonce" => $nonces['salt'],
+        // on the other hand, include these here even if you leave them out of the JS init
+        "postbackUrl" => $request['postbackUrl'], // if not specified in the JS init, defaults to the empty string
+        "environment" => $request['environment'], // defaults to "cert"
+        "preAuth" => $request['preAuth'] // defaults to false
+    ]; 
     
-    $combinedString = $requestType . $requestId . $merchantCredentials['MID'] . $postbackUrl . $nonce . $amount;
-    $authKey = createHmac($combinedString, $merchantCredentials["MKEY"]);
-
+    $authKey = getAuthKey(json_encode($req), $developer['KEY'], $nonces['salt'], $nonces['iv']);
 ?>
+
 <div class="wrapper text-center">
     <h1>RequireJS</h1>
     <p>The <code>pay.js</code> and <code>pay.min.js</code> files use a bundled version of <a href="http://requirejs.org/">RequireJS</a> to manage module dependencies. If you already use RequireJS on your site, reference PayJS directly via <code>config.paths</code>:</p>
@@ -20,11 +29,11 @@
 <pre><code>        requirejs.config({
             paths: {
                 "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-                <span style="background-color: yellow">"PayJS": 'https://www.sagepayments.net/pay/js/build'</span>
+                <span style="background-color: yellow">"PayJS": 'https://www.sagepayments.net/pay/1.0.0/js/build'</span>
             },
         });</code></pre>
 <div class="wrapper text-center">
-    <p>And then use it as you normally would:</p>
+    <p>And then use it just like you would use any other module:</p>
 </div>
 <pre><code>        requirejs(['jquery', 'myAwesomeCode', <span style="background-color: yellow">'PayJS/UI'</span>],
         function($, $MAC, $UI) {
@@ -48,31 +57,39 @@
         baseUrl: "require/my/other/js",
         paths: {
             "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-            "PayJS": 'https://www.sagepayments.net/pay/js/build',
+            "PayJS": 'https://www.sagepayments.net/pay/1.0.0/js/build',
         },
     });
-
-    requirejs(['jquery', 'myAwesomeCode', 'PayJS/UI'],
-    function($, $MAC, $UI) {
-        $MAC.doBusiness();
+</script>
+<script type="text/javascript">
+    // you guessed it -- PayJS() is just an alias of requirejs()
+    requirejs(['myAwesomeCode', 'PayJS/UI', 'jquery'],
+    function($MAC, $UI, $) {
+        $MAC.doBusiness(); 
         $UI.Initialize({
-            apiKey: "<?php echo $developerId; ?>",
-            merchantId: "<?php echo $merchantCredentials['MID']; ?>",
+            apiKey: "<?php echo $developer['ID']; ?>",
+            environment: "<?php echo $request['environment']; ?>",
+            postbackUrl: "<?php echo $request['postbackUrl']; ?>",
+            merchantId: "<?php echo $merchant['ID']; ?>",
             authKey: "<?php echo $authKey; ?>",
+            nonce: "<?php echo $nonces['salt']; ?>",
             requestType: "<?php echo $requestType; ?>",
             requestId: "<?php echo $requestId; ?>",
-            amount: "<?php echo $amount; ?>",
+            amount: "<?php echo $request['amount']; ?>",
+            billing: {
+                name: "PaymentsJS Sample",
+                street: "123 Address St",
+                city: "Denver",
+                state: "CO",
+                zip: "80205"
+            },
             elementId: "paymentButton",
-            debug: true,
-            postbackUrl: "<?php echo $postbackUrl; ?>",
             phoneNumber: "1-800-555-1234",
-            nonce: "<?php echo $nonce; ?>",
-            //suppressResultPage: true
         });
-        $UI.setCallback(function(resp) {
-            console.log(resp.getResponse());
+        $UI.setCallback(function($RESP) {
+            console.log($RESP.getResponse());
             $("#paymentResponse").text(
-                resp.getResponse({ "json": true })
+                $RESP.getResponse({ "json": true })
             );
         });
     });
