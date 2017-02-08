00f40bf4ee5866bbd441747aa69509058e27125a
majidmade <majid.razvi@sage.com>
2017-02-07 19:24:15 -0500

    first functional node sample

--- Samples/node/routes/index.js
+++ Samples/node/routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const payjs = require('../services/sagePayments.js');
+const express = require('express');
+const router = express.Router();
 
 router.get('/:page', (req, res, next) => {
-  res.render(req.params.page, {})
+  res.render(req.params.page, { initialization: JSON.stringify(payjs.getInitialization()) })
 })
-
 
 router.get('/', (req, res, next) => {
   res.render('index');
---
+++ Samples/node/services/sagePayments.js
@@ -0,0 +1,61 @@
+const crypto = require('crypto-js');
+
+const config = {
+    merchant: {
+        id: "999999999997",
+        key: "K3QD6YWyhfD",
+    },
+    developer: {
+        id: "GTq2h4mXxLIBtzbOWLO2GwqZfOgK8BbT",//"7SMmEF02WyC7H5TSdG1KssOQlwOOCagb",
+        key: "ICkrA2n6HIleJ663",//"wtC5Ns0jbtiNA8sP",
+    },
+    postbackUrl: "https://www.example.com/myHandler.php", // https://requestb.in is great for playing with this
+    environment: "cert",
+    amount: "1.00",
+    preAuth: "false",
+    requestType: "payment",
+}
+
+
+const baseRequest = {
+  clientId: config.developer.id,
+  postbackUrl: config.postbackUrl, // you get a copy of the response here
+  merchantId: config.merchant.id,
+  authKey: undefined,
+  salt: undefined,
+  requestType: config.requestType,
+  orderNumber: undefined,
+  amount: config.amount,
+};
+
+function getAuthedRequest(){
+    const newRequest = baseRequest;
+    const nonces = getSecureNonces();
+    newRequest.orderNumber =  Date.now().toString();
+    newRequest.salt = nonces.salt;
+    newRequest.merchantKey = config.merchant.key;
+    newRequest.authKey = getAuthKey(JSON.stringify(newRequest), nonces, config.developer.key);
+    delete newRequest.merchantKey;
+    return newRequest;
+}
+
+function getSecureNonces(){
+    const iv = crypto.lib.WordArray.random(16)
+    const salt = crypto.enc.Base64.stringify(crypto.enc.Utf8.parse(crypto.enc.Hex.stringify(iv)));
+    return {
+        iv: iv,
+        salt: salt
+    }
+}
+
+function getAuthKey(message, nonces, secret){
+    var derivedPassword = crypto.PBKDF2(secret, nonces.salt, { keySize: 256/32, iterations: 1500, hasher: crypto.algo.SHA1 });
+    var encrypted = crypto.AES.encrypt(message, derivedPassword, { iv: nonces.iv });
+    return encrypted.toString();
+}
+
+module.exports = {
+    getInitialization: () => {
+        return getAuthedRequest();
+    }
+}
