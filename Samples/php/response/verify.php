f07fe339ed18f726a92c297f7a990b63df471573
Majid Razvi <majid.razvi@sage.com>
2017-03-27 17:59:57 -0400

    fix to php sample

--- Samples/php/response/verify.php
+++ Samples/php/response/verify.php
@@ -1,6 +1,6 @@
 <?php
     require('../shared/shared.php');
-    $req = json_decode(file_get_contents('php://input'));
+    $req = file_get_contents('php://input');
     
     $results = [
         hash => getHmac($req, $developer['KEY']),
